130e6859863548d6a5d725d699addca8c5e6ad21
Cynthia Mumbua Muinde <cynmumbua@gmail.com>
2019-08-24 14:45:45 +0300

    feature: Create new session

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,14 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const sessions= require('../models/sessions');
+const Middleware= require('../middleware/middleware');
+const mentorsInfo= require('../models/mentorsInfo');
+const Joi= require('joi');
+
+
+router.post('/', Middleware.checkUserToken, (request, response)=>{
+	const{error}= validateQuestion(request.body);
+	if(error){
+		return response.status(409).json({
+			message: error.details[0].message
+		});
+	}
+
+	const checkMentor= mentorsInfo.find(mentorsInfo=>mentorsInfo.userId == request.body.mentorId);
+
+	if(checkMentor){
+		
+		const newSession ={
+			sessionId: sessions.length + 1,
+			mentorId: request.body.mentorId,
+			menteeId: request.user.userId,
+			questions: request.body.questions,
+			menteeEmail: request.user.email,
+			status: 'pending'
+		}
+    sessions.push(newSession);
+    response.status(201).json({
+    	status: 201,
+    	data: newSession
+    });
+	}else{
+		 response.status(404).json({
+    	 status: 404,
+    	 message: 'No mentor with that Id'
+    });
+	}
+
+});
 
 router.get('/', (request, response)=>{
 
-	response.send('This is my first app');
-});
-
-router.post('/', (request, response)=>{
-
-	response.send('This is my first app');
+	
 });
 
 router.patch('/:sessionId/accept', (request, response)=>{
@@ -31,4 +65,11 @@ router.delete('/:sessionId/delete', (request, response)=>{
 	response.send('This is my first app');
 });
 
+const validateQuestion =(formData)=> {
+	schema = {
+		mentorId:Joi.number().required(),
+		questions: Joi.string().required().min(6)
+	}
+	return Joi.validate(formData, schema);
+}
 module.exports= router;
