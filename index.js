67b9142241e138e026cc7369149bb977853fdada
Cynthia Mumbua Muinde <cynmumbua@gmail.com>
2019-08-24 15:01:59 +0300

    chore(eslint) refactor index.js

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-//import express
+// import express
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 5000;
 const auth = require('./routes/auth');
 const mentors = require('./routes/mentors');
-const sessions =require('./routes/sessions');
+const sessions = require('./routes/sessions');
 const admin = require('./routes/admin');
+
 app.use(express.json());
 
 app.use('/api/v1/auth', auth);
 
-app.patch('/api/v1/user/:userId',admin );
+app.patch('/api/v1/user/:userId', admin);
 
 app.use('/api/v1/mentors', mentors);
 
 app.use('/api/v1/sessions', sessions);
 
 
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.listen(port, ()=> console.log(`Listening on port ${port}`));
-
-module.exports= app;
+module.exports = app;
