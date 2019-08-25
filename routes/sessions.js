8414cec69d0221f7803534dc27bd3b244da401bc
Cynthia Mumbua Muinde <cynmumbua@gmail.com>
2019-08-25 22:02:39 +0300

    feature: Add accept a session request functionality

--- routes/sessions.js
+++ routes/sessions.js
@@ -63,16 +63,33 @@ router.get('/',Middleware.checkUserToken, (request, response)=>{
 
 	}
 	else{
-		response.status(409).json({
+		response.status(401).json({
 			message: 'Unauthorised access'
 		});
 	}
 	
 });
 
-router.patch('/:sessionId/accept', (request, response)=>{
-
-	response.send('This is my first app');
+router.patch('/:sessionId/accept',Middleware.checkUserToken, (request, response)=>{
+	const userId =request.user.userId;
+	if(request.user.mentor == true){
+			const acceptSession = sessions.find(sessions=>sessions.sessionId == request.params.sessionId);
+			if(acceptSession){
+				acceptSession.status = 'accepted';
+				response.status(200).json({
+		    	status: 200,
+		    	data: acceptSession
+			});	
+			}else{
+				response.status(404).json({
+					message: 'session not found'
+				});
+			}	
+	}else{
+		response.status(401).json({
+		message: 'Unauthorised access'
+	});
+	}
 });
 
 router.patch('/:sessionId/reject', (request, response)=>{
